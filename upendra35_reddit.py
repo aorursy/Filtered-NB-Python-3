580c439d776b79ca71bd1bf9ecb19192e5a17253
aoreyv <urisayar.kae@student.mahidol.ac.th>
2023-02-27 23:17:01 +0700

    update nb

--- upendra35_reddit.py
+++ upendra35_reddit.py
@@ -21,23 +21,23 @@ def convert(data):
 
 reddit=convert(train)
 
-reddit['is_reddit'] = np.random.uniform(0, 1, len(reddit)) <= .75
-reddit, validate = reddit[reddit['is_reddit']==True], reddit[reddit['is_reddit']==False]
+# reddit['is_reddit'] = np.random.uniform(0, 1, len(reddit)) <= .75
+# reddit, validate = reddit[reddit['is_reddit']==True], reddit[reddit['is_reddit']==False]
 
-lg = LogisticRegression()
-lg.fit(x_train, y_train)
+# lg = LogisticRegression()
+# lg.fit(x_train, y_train)
 
-Disbursed_lg=lg.predict_proba(x_validate)
+# Disbursed_lg=lg.predict_proba(x_validate)
 
-fpr, tpr, _ = roc_curve(y_validate, Disbursed_lg[:,1])
-roc_auc = auc(fpr, tpr)
-print roc_auc
+# fpr, tpr, _ = roc_curve(y_validate, Disbursed_lg[:,1])
+# roc_auc = auc(fpr, tpr)
+# print roc_auc
 
-rf = RandomForestClassifier()
-rf.fit(x_train, y_train)
-disbursed = rf.predict_proba(x_validate)
-fpr, tpr, _ = roc_curve(y_validate, disbursed[:,1])
-roc_auc = auc(fpr, tpr)
-print roc_auc
+# rf = RandomForestClassifier()
+# rf.fit(x_train, y_train)
+# disbursed = rf.predict_proba(x_validate)
+# fpr, tpr, _ = roc_curve(y_validate, disbursed[:,1])
+# roc_auc = auc(fpr, tpr)
+# print roc_auc
 
 
