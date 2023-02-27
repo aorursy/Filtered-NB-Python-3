22466f701466765f8fc584bde8c4d0d59770569d
aoreyv <urisayar.kae@student.mahidol.ac.th>
2023-02-27 23:09:29 +0700

    update nb

--- rudiwowot_day-1-machine-learning-rudi-s.py
+++ rudiwowot_day-1-machine-learning-rudi-s.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
-
 # This Python 3 environment comes with many helpful analytics libraries installed
 # It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
 # For example, here's several helpful packages to load
@@ -20,48 +18,22 @@ for dirname, _, filenames in os.walk('/kaggle/input'):
 
 # You can write up to 5GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
 # You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
-
-
-
 df = pd.read_csv('/kaggle/input/titanic/train.csv')
 df.head(5)
 
-
 df.groupby('Sex').size()/len(df)*100
-
-
-
 
 df['Cabin'].unique()
 
-
-
-
 len(df[df["Parch"] >= 2])
-
-
-
-
-df[(df["Parch"] == 0) & (df["SibSp"] == 0)][]
-
-
 
 
 # highest value fare
 
 df[df["Fare"] == max(df["Fare"])]["Embarked"].unique()[0]
 
-
-
-
 df.groupby("Embarked")["Fare"].mean()
 
-
-
-
 df[df['Age'] >= 50].groupby('Cabin').size().sort_values(ascending=False)
-
-
-
 
 df[['Pclass', 'Embarked', 'Sex', 'Survived']].groupby(['Pclass', 'Embarked', 'Sex']).agg(['count', 'sum', 'mean'])
